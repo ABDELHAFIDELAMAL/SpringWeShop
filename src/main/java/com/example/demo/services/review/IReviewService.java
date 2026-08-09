17c411533ce6c966d6ba7d4dc843a80e1a67bb10
Abdelhafid ELAMAL <elamal.a980@ucd.ac.ma>
2026-08-09 22:00:15 +0100

    Implents methos IReviewService

--- src/main/java/com/example/demo/services/review/IReviewService.java
+++ src/main/java/com/example/demo/services/review/IReviewService.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface IReviewService {
     List<Review> getAllReviews();
+    Review addReview(Review review);
+    Review getReviewById(Long id);
+    List<Review> getReviewsByProductId(Long productId);
+    List<Review> getReviewsByUserId(Long userId);
+    Review updateReview(Long id, Review review);
+    void deleteReview(Long id);
+    Double getAverageRatingByProductId(Long productId);
+    long getReviewCountByProductId(Long productId);
 }
--- src/main/java/com/example/demo/services/review/ReviewService.java
+++ src/main/java/com/example/demo/services/review/ReviewService.java
@@ -16,4 +16,44 @@ public class ReviewService implements IReviewService{
     public List<Review> getAllReviews() {
         return reviewRepository.findAll();
     }
+
+    @Override
+    public Review addReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public Review getReviewById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Review> getReviewsByProductId(Long productId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Review> getReviewsByUserId(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public Review updateReview(Long id, Review review) {
+        return null;
+    }
+
+    @Override
+    public void deleteReview(Long id) {
+
+    }
+
+    @Override
+    public Double getAverageRatingByProductId(Long productId) {
+        return 0.0;
+    }
+
+    @Override
+    public long getReviewCountByProductId(Long productId) {
+        return 0;
+    }
 }
